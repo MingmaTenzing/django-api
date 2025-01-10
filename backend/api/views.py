08af52cd59dcb303da3e3eb54442feb51a7493b0
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2025-01-10 16:06:48 +0800

    drf

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.models import Product
+from django.forms.models import model_to_dict
+from django.http import HttpResponse
 import json
+
+from products.serializers import ProductSerializer
 # Create your views here.
 
 
-
+@api_view(['POST', 'GET'])
 def api_home(request, *args, **kwargs):
-    body = request.body
-    print(type(body))
-    data = json.loads(body)
-    print(request.headers)
-    return JsonResponse(data)
+    print(request.body)
+    serialize = ProductSerializer(data=request.data)
+    if serialize.is_valid():
+        instance = serialize.save()
+        print(serialize.data)
+        return Response(serialize.data)
+    
+    # instance = Product.objects.all()
+    # print(type(instance))
+    # data = {}
+    
+    # if instance: 
+    #     data = ProductSerializer(instance, many=True).data
+    
+    return Response('not working')
+
+
+    
---
+++ backend/products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title= models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.9)
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+    @property
+    def sale_price(self):
+        return "%.2f"%(float(self.price) * 0.8)
+    
+    def discount_percentage(self):
+        return "10"
