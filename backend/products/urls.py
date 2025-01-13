668318ca377147f8a797afa79b1974334f2bdd8a
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2025-01-14 06:00:04 +0800

    learning more about views

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import Product_detail_view, Product_create_api_view
+from .views import products_model_viewset,Product_detail_view, Product_create_api_view, Product_list_view, alt_api_view
 
+from rest_framework import routers
 
+# router = routers.DefaultRouter()
+
+# router.register(r'viewset', products_model_viewset)
 urlpatterns = [
-    path("", Product_create_api_view.as_view()),
-    path("<int:pk>", Product_detail_view.as_view())
+    # path("", include(router.urls)),
+    path("list", Product_list_view.as_view()),
+    path("create", Product_create_api_view.as_view()),
+    path("<int:pk>", Product_detail_view.as_view()),
+    path("alt", alt_api_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -10,8 +11,37 @@ class Product_detail_view(generics.RetrieveAPIView):
     
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+class Product_list_view(generics.ListAPIView): 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
 class Product_create_api_view(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+
+
+@api_view(['GET', 'POST'])
+def alt_api_view(request):
+    if request.method == 'GET':
+        query_instance = Product.objects.all() 
+        serializer = ProductSerializer(query_instance, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        if not request.data: 
+            return Response('no data provided please provide through body')
+        seriliaze_data = ProductSerializer(data=request.data)
+        if seriliaze_data.is_valid():
+            seriliaze_data.save()
+            print(seriliaze_data)
+            return Response(seriliaze_data.data)
+        return Response("Wrong data format please check docs")
+
+
+class products_model_viewset(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
